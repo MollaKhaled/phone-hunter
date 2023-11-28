a6706c5d0b5d9652288b6196ecdab21e9260fb44
Mollakhaled <khaledbalok@gmail.com>
2023-11-28 14:19:22 +0600

    loader added

--- js/app.js
+++ js/app.js
@@ -1,32 +1,30 @@
 const loadPhones = async (searchText) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  const  res = await fetch(url);
+  const res = await fetch(url);
   const data = await res.json();
   displayPhones(data.data)
 }
-const displayPhones = phones =>{
+const displayPhones = phones => {
   const phonesContainer = document.getElementById('phones-container');
   phonesContainer.innerHTML = '';
   // display 20 phone only
-  phones = phones.slice(0,10);
+  phones = phones.slice(0, 10);
 
   // display no phones
-const noPhone = document.getElementById('no-found-message');
-if(phones.length === 0){
-  noPhone.classList.remove('d-none')
-}
-else{
-  noPhone.classList.add('d-none')
-}
+  const noPhone = document.getElementById('no-found-message');
+  if (phones.length === 0) {
+    noPhone.classList.remove('d-none')
+  }
+  else {
+    noPhone.classList.add('d-none')
+  }
 
   // display all phones
 
-
-
-   phones.forEach(phone =>{
-   const  phoneDiv = document.createElement('div');
-   phoneDiv.classList.add('col');
-   phoneDiv.innerHTML = `
+  phones.forEach(phone => {
+    const phoneDiv = document.createElement('div');
+    phoneDiv.classList.add('col');
+    phoneDiv.innerHTML = `
     <div class="col">
    <div class="card h-100">
      <img src="${phone.image}" class="rounded mx-auto d-block m-4 p-4" alt="...">
@@ -39,12 +37,27 @@ else{
  </div>
    `;
     phonesContainer.appendChild(phoneDiv);
-   })
-} 
-document.getElementById('search-button').addEventListener('click', function(){
+  });
+  // stop loader
+  toggleSpinner(false);
+}
+document.getElementById('search-button').addEventListener('click', function () {
+  // start loader
+  toggleSpinner(true);
   const searchField = document.getElementById('search-field');
-  const searchText = searchField.value ;
+  const searchText = searchField.value;
   loadPhones(searchText)
   document.getElementById('search-field').value = '';
 })
+
+const toggleSpinner = isLoading => {
+  const loaderSection = document.getElementById('loader');
+  if(isLoading){
+    loaderSection.classList.remove('d-none')
+  }
+  else{
+    loaderSection.classList.add('d-none')
+  }
+}
+
 // loadPhones();
