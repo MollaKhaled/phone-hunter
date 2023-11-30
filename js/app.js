1395f14acc0444ac8f7e66e814f6960453db5ad8
Mollakhaled <khaledbalok@gmail.com>
2023-11-30 19:35:15 +0600

    Dynamic API Data Load and search on keyboard enter button

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-const loadPhones = async (searchText) => {
+const loadPhones = async (searchText, dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayPhones(data.data)
+  displayPhones(data.data, dataLimit)
 }
-const displayPhones = phones => {
+const displayPhones = (phones, dataLimit) => {
   const phonesContainer = document.getElementById('phones-container');
-  phonesContainer.innerHTML = '';
-  // display 20 phone only
-  phones = phones.slice(0, 10);
-
+  phonesContainer.textContent = '';
+  // display 10 phone only
+  const showAll = document.getElementById('show-all');
+  if (dataLimit && phones.length > 10) {
+    phones = phones.slice(0, 10);
+    showAll.classList.remove('d-none')
+  }
+  else {
+    showAll.classList.add('d-none')
+  }
   // display no phones
   const noPhone = document.getElementById('no-found-message');
   if (phones.length === 0) {
@@ -32,6 +38,7 @@ const displayPhones = phones => {
        <h4 class="card-title">${phone.phone_name}</h4>
        <h5 class="card-title">${phone.brand}</h5>
        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+       <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
      </div>
    </div>
  </div>
@@ -41,23 +48,44 @@ const displayPhones = phones => {
   // stop loader
   toggleSpinner(false);
 }
-document.getElementById('search-button').addEventListener('click', function () {
-  // start loader
+const processSearch = (dataLimit) => {
   toggleSpinner(true);
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
-  loadPhones(searchText)
-  document.getElementById('search-field').value = '';
+  loadPhones(searchText, dataLimit)
+  
+}
+document.getElementById('search-button').addEventListener('click', function () {
+  // start loader
+  processSearch(10);
 })
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e){ 
+  if (e.key === 'Enter'){
+    processSearch(10);
+  }
+});
 
 const toggleSpinner = isLoading => {
   const loaderSection = document.getElementById('loader');
-  if(isLoading){
+  if (isLoading) {
     loaderSection.classList.remove('d-none')
   }
-  else{
+  else {
     loaderSection.classList.add('d-none')
   }
+}
+// not the best way to showAll
+  document.getElementById('btn-show-all').addEventListener('click', function () {
+  processSearch();
+})
+
+const loadPhoneDetails = async(id) =>{
+  const url=`https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.data) ;
 }
 
 // loadPhones();
